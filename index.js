d34d4480d4b3a1e49f8d3061d5d503fb632dfa02
Carlos <carresan.val@gmail.com>
2023-10-13 01:00:09 +0200

    NEW: Creado proyecto base.

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const db = require('./db');
+const router = require('./router');
+const app = express();
+const PORT = 7000;
+
+
+app.use(express.json());
+app.use(router);
+
+// SERVER CONTECTION CHECK
+
+app.get('/health', (req,res)=>  {
+    return res.send("healthy");
+})
+
+// DDBB CONECTION CHECK
+
+db.then(() =>
+    {
+        app.listen(PORT, () => {
+            console.log('Server is running on port: ' + PORT);
+        })
+    }
+).catch((error) => {
+    console.error('Error starting server', error.message);
+})
+
+module.exports = app;
---
+++ router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const cors = require('cors');
+
+
+router.use(cors());
+
+
+module.exports = router;
