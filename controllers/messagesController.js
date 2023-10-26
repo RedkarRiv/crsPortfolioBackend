d1d5d9e14f4a9350b1c65a87b44bbc9a29468c8d
Carlos <carresan.val@gmail.com>
2023-10-26 20:10:09 +0200

    FIX: Validación por IP para contactos máximos/h implementado con éxito

--- controllers/messagesController.js
+++ controllers/messagesController.js
@@ -1,18 +1,58 @@
 const { ContactData } = require("../models");
 
+const requestLimitPerHour = 2; // Número máximo de solicitudes por hora
+const requests = {}; // Objeto para realizar un seguimiento de las solicitudes
+
 const messageController = {};
 
 messageController.newContact = async (req, res) => {
   try {
+    const contactName = req.body.name;
+    const contactEmail = req.body.email;
+    const contactMessage = req.body.message;
+    const clientIP = req.clientIp; // Obtiene la IP del cliente
+
+    // Verifica si el cliente ha realizado solicitudes en la última hora
+    if (!requests[clientIP]) {
+      requests[clientIP] = [];
+    }
+
+    const currentTime = new Date();
+    const filteredRequests = requests[clientIP].filter((time) => {
+      return currentTime - time <= 3600000; // Elimina solicitudes anteriores a una hora
+    });
+
+    console.log("Esto es el client IP:", clientIP);
+    console.log("Esto son las solicitudes previas:", requests[clientIP]);
+    console.log("Esto son las solicitudes filtradas:", filteredRequests);
+
+    // Verifica si el límite de solicitudes por hora se ha alcanzado
+    if (filteredRequests.length >= requestLimitPerHour) {
+      return res.status(429).json({
+        success: false,
+        message: "Demasiadas solicitudes en la última hora.",
+      });
+    }
+
+    // Registra la nueva solicitud
+    requests[clientIP] = [...filteredRequests, currentTime];
+
+
+    const newMessage = await ContactData.create({
+      name: contactName,
+      email: contactEmail,
+      message: contactMessage,
+    });
+
     return res.json({
       success: true,
-      message: "Usuarios importados correctamente",
-      data: allUsers,
+      message: "Mensaje enviado correctamente",
+      data: newMessage,
     });
   } catch (error) {
     return res.status(500).json({
       success: false,
-      message: "Error al importar usuarios",
+      message: "Error al enviar el mensaje",
       error: error.message,
     });
   }
--- router.js
+++ router.js
@@ -3,11 +3,13 @@ const cors = require("cors");
 const authRoutes = require("./views/authRoutes");
 const adminRoutes = require("./views/adminRoutes");
 const userRoutes = require("./views/userRoutes");
+const contactRoutes = require("./views/contactRoutes");
 
 router.use(cors());
 
 router.use("/auth", authRoutes);
 router.use("/admin", adminRoutes);
 router.use("/user", userRoutes);
+router.use("/message", contactRoutes);
 
 module.exports = router;
